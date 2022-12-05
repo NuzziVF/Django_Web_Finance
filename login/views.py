2b7d4f03df1fe62dc2137e6fa28c0849172779cb
NuzziVF <dylan.mc.cervantes7@gmail.com>
2022-12-05 15:10:24 -0600

    Revert "moved things around"
    This reverts commit 7c27ae65ad20539a05e1393683293aba9534396e.

--- login/views.py
+++ login/views.py
@@ -213,26 +213,6 @@ def editPage(request):
 
 
 @login_required(login_url="login")
-def reviewsPage(request):
-    try:
-        name = request.user
-        user = find_person(name)
-        if len(find_transaction(name)) == 0:
-            print("No transactions found")
-            context = {"user": user}
-        else:
-            transactions = find_transaction(name)
-            print(f"{len(transactions)} transactions found")
-            transactions = transactions.reverse()
-            context = {"user": user, "transactions": transactions}
-        return render(request, "reviews.html", context)
-    except Person.DoesNotExist:
-        user = find_name(request.user)
-        context = {"user": None}
-        return render(request, "reviews.html", context)
-
-
-@login_required(login_url="login")
 def savingsPage(request):
     return redirect("home")
 
@@ -340,11 +320,30 @@ def import_data():
 
 
 def transaction(a, b, c):
-    PERSON_FOUND = find_person(c)
-    trans = Banking_Changes(name=a, cost=b, user=PERSON_FOUND)
+    q = find_person(c)
+    trans = Banking_Changes(name=a, cost=b, user=q)
     trans.save()
     print(trans)
     print(q.budget.balance)
-    PERSON_FOUND.budget.balance -= int(trans.cost)
-    PERSON_FOUND.budget.save()
-    print(PERSON_FOUND.budget.balance)
+    q.budget.balance -= int(trans.cost)
+    q.budget.save()
+    print(q.budget.balance)
+
+
+@login_required(login_url="login")
+def reviewsPage(request):
+    try:
+        name = request.user
+        user = find_person(name)
+        if len(find_transaction(name)) == 0:
+            print("No transactions found")
+            context = {"user": user}
+        else:
+            transactions = find_transaction(name)
+            print(f"{len(transactions)} transactions found")
+            context = {"user": user, "transactions": transactions}
+        return render(request, "reviews.html", context)
+    except Person.DoesNotExist:
+        user = find_name(request.user)
+        context = {"user": None}
+        return render(request, "reviews.html", context)
